8931b6de9db29345bea0bbaa7cbf821c9f99dbf8
Jisan Mia <jisanmia47@gmail.com>
2020-04-29 08:49:40 +0600

    shopping cart ended

--- script.js
+++ script.js
@@ -1,65 +1,59 @@
 //1st plus sign event handler--->
 const firstPlusSign = document.getElementById("1st-plusSign");
 firstPlusSign.addEventListener("click", function(){
-    const updateValue= floatingValue("updateValue");
-    updatedValue(updateValue, "updateValue", 1);
-    const currentIphone = floatingInnerText("currentIphone");
-    iphoneAndCoverValue(currentIphone, 1219, "currentIphone");
-    const subtotal = floatingInnerText("subtotal");
-    iphoneAndCoverValue(subtotal, 1219, "subtotal" );
-    const tax = floatingInnerText("tax");
-    iphoneAndCoverValue(tax, 5, "tax");
-})
-//1st minus sign event handler 
-const firstMinusSign = document.getElementById("1st-minusSign");
-firstMinusSign.addEventListener("click", function(){
-    const degradeValue = floatingValue("updateValue");
-    updatedValue(degradeValue, "updateValue", -1);
-    const currentIphone = floatingInnerText("currentIphone");
-    iphoneAndCoverValue(currentIphone, -1219, "currentIphone");
-    const subtotal = floatingInnerText("subtotal");
-    iphoneAndCoverValue(subtotal,-1219, "subtotal" )
-    const tax = floatingInnerText("tax");
-    iphoneAndCoverValue(tax, -5, "tax");
-
+    allSign("updateValue", 1, "currentIphone", 1219, 5);
 })
 //second plus sign event handler
 const secondPlusSign = document.getElementById("2nd-plusSign");
 secondPlusSign.addEventListener("click", function(){
-    const coverUpdate = floatingValue("coverUpdate");
-    updatedValue(coverUpdate, "coverUpdate", 1);
-    const currentCover = floatingInnerText("currentCover");
-    iphoneAndCoverValue(currentCover, 59, "currentCover");
-    const subtotal = floatingInnerText("subtotal");
-    iphoneAndCoverValue(subtotal,59, "subtotal" )
-    const tax = floatingInnerText("tax");
-    iphoneAndCoverValue(tax, 5, "tax");
+    allSign("coverUpdate", 1, "currentCover", 59, 5);
 })
+
+//1st minus sign event handler 
+const firstMinusSign = document.getElementById("1st-minusSign");
+firstMinusSign.addEventListener("click", function(){
+    allSign("updateValue", -1, "currentIphone", -1219, -5);
+})
+
+
 //second minus sign event handler
 const secondMinusSign = document.getElementById("2nd-minusSign");
 secondMinusSign.addEventListener("click", function(){
-    const coverUpdate = floatingValue("coverUpdate");
-    updatedValue(coverUpdate, "coverUpdate", -1);
-    const currentCover = floatingInnerText("currentCover");
-    iphoneAndCoverValue(currentCover, -59, "currentCover")
-    const subtotal = floatingInnerText("subtotal");
-    iphoneAndCoverValue(subtotal,-59, "subtotal" )
-    const tax = floatingInnerText("tax");
-    iphoneAndCoverValue(tax, -5, "tax");
-    
+    allSign("coverUpdate", -1, "currentCover", -59, -5);
 })
+
+
+//current values, updated value and tax value function 
+function currentValues(currentValue, value, id, taxValue){
+    iphoneAndCoverValue(currentValue, value, id);
+    const subtotal = floatingInnerText("subtotal");
+    iphoneAndCoverValue(subtotal,value, "subtotal" )
+    const tax = floatingInnerText("tax");
+    iphoneAndCoverValue(tax, taxValue, "tax");
+}
+
+// all the plus minus sign function:
+function allSign(id, value, id2, value2, value3){
+    const coverUpdate = floatingValue(id);
+    updatedValue(coverUpdate, id, value);
+    const currentCover = floatingInnerText(id2);
+    currentValues(currentCover, value2, id2, value3 );
+}
+
 //get and float the input value (function)
 function floatingValue(id){
     const number = document.getElementById(id).value;
     const floatingNumber = parseFloat(number);
     return floatingNumber;
 }
+
 //get and float the innerHTML (function)
 function floatingInnerText(id){
     const number = document.getElementById(id).innerHTML;
     const floatingNumber = parseFloat(number);
     return floatingNumber;
 }
+
 //update or degrade number value
 function updatedValue(value, id, addValue){
     const currentValue = value+ addValue;
@@ -80,5 +74,15 @@ function iphoneAndCoverValue(value, addValue, id){
     document.getElementById(id).innerHTML = updateValue;
     }
 }
+// check out button handler:
+const checkOutBtn = document.querySelector("#checkOutBtn");
+checkOutBtn.addEventListener("click", function(){
+    const sectionPart = document.querySelector("#section");
+    sectionPart.style.display = "none"
+    const secTwo = document.querySelector("#sectionTwo")
+    secTwo.style.display = "block";
+
+})
+
 
 
